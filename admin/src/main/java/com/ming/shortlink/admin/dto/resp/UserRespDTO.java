46e7dfe76f365e33c310d19e7bdd9d6d9b352a24
clownming <1239064816@qq.com>
2024-01-16 17:52:12 +0800

    开发用户敏感信息脱敏接口

--- admin/src/main/java/com/ming/shortlink/admin/dto/resp/UserRespDTO.java
+++ admin/src/main/java/com/ming/shortlink/admin/dto/resp/UserRespDTO.java
@@ -1,5 +1,7 @@
 package com.ming.shortlink.admin.dto.resp;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.ming.shortlink.admin.common.serialize.PhoneDesensitizationSerializer;
 import lombok.Data;
 
 /**
@@ -27,6 +29,7 @@ public class UserRespDTO {
     /**
      * 手机号
      */
+    @JsonSerialize(using = PhoneDesensitizationSerializer.class)
     private String phone;
 
     /**

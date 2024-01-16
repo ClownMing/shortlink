6813fe63bf687fe3b00c4665bb627cd21a84a366
clownming <1239064816@qq.com>
2024-01-16 21:12:09 +0800

    通过redisson分布式锁完善用户注册功能，防止高并发的注册

---
+++ admin/src/main/java/com/ming/shortlink/admin/common/constant/RedisCacheConstant.java
@@ -0,0 +1,10 @@
+package com.ming.shortlink.admin.common.constant;
+
+/**
+ * @author clownMing
+ * short-link后台管理 Redis 缓存常量类
+ */
+public class RedisCacheConstant {
+
+    public static final String LOCK_USER_REGISTER_KEY = "short-link:lock_user-register:";
+}

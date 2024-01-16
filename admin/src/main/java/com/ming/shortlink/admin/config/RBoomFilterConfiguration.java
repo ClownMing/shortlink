3d3a4e77968979fb62fc45232ec64f51f2a48c09
clownming <1239064816@qq.com>
2024-01-16 20:55:07 +0800

    进行redis服务配置

---
+++ admin/src/main/java/com/ming/shortlink/admin/config/RBoomFilterConfiguration.java
@@ -0,0 +1,26 @@
+package com.ming.shortlink.admin.config;
+
+import org.redisson.api.RBloomFilter;
+import org.redisson.api.RedissonClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author clownMing
+ */
+@Configuration
+public class RBoomFilterConfiguration {
+
+    /**
+     * 防止用户注册查询数据库的布隆过滤器
+     */
+    @Bean
+    public RBloomFilter<String> userRegisterCachePenetrationBloomFilter(RedissonClient redissonClient) {
+        RBloomFilter<String> cachePenetrationBloomFilter = redissonClient.getBloomFilter("userRegisterCachePenetrationBloomFilter");
+        cachePenetrationBloomFilter.tryInit(100000000L, 0.001);
+        return cachePenetrationBloomFilter;
+    }
+
+
+
+}

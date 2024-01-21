48a70020960b4aae8eb01def746dcdb44530d212
clownming <1239064816@qq.com>
2024-01-21 19:19:09 +0800

    短链接后台管理系统-删除短链接分组功能

--- admin/src/main/java/com/ming/shortlink/admin/service/GroupService.java
+++ admin/src/main/java/com/ming/shortlink/admin/service/GroupService.java
@@ -28,4 +28,9 @@ public interface GroupService extends IService<GroupDO> {
      * 修改短链接分组
      */
     void updateGroup(ShortLinkGroupUpdateDTO requestParam);
+
+    /**
+     * 删除短链接分组
+     */
+    void deleteGroup(String gid);
 }

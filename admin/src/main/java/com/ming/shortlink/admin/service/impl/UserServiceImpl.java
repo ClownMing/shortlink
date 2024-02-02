f6ca0927eaa660d922a50aa7ba3e391621a121b0
clownming <1239064816@qq.com>
2024-02-02 15:25:10 +0800

    优化：用户已登录状态如果重复登录返回Token

--- admin/src/main/java/com/ming/shortlink/admin/service/impl/UserServiceImpl.java
+++ admin/src/main/java/com/ming/shortlink/admin/service/impl/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.ming.shortlink.admin.service.impl;
 
 import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.collection.CollUtil;
 import com.alibaba.fastjson2.JSON;
 import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
@@ -26,6 +27,7 @@ import org.springframework.dao.DuplicateKeyException;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
@@ -111,9 +113,13 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
         if (dbUser == null) {
             throw new ClientException("用户不存在");
         }
-        Boolean hasLoginKey = stringRedisTemplate.hasKey("login_" + requestParam.getUsername());
-        if (hasLoginKey != null && hasLoginKey) {
-            throw new ClientException("用户已登录");
+        Map<Object, Object> hasLoginMap = stringRedisTemplate.opsForHash().entries("login_" + requestParam.getUsername());
+        if (CollUtil.isNotEmpty(hasLoginMap)) {
+            String token = hasLoginMap.keySet().stream()
+                    .findFirst()
+                    .map(Object::toString)
+                    .orElseThrow(() -> new ClientException("用户登录错误"));
+            return new UserLoginRespDTO(token);
         }
         String uuid = UUID.randomUUID().toString();
         stringRedisTemplate.opsForHash().put("login_" + requestParam.getUsername(), uuid, JSON.toJSONString(dbUser));

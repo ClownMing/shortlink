aaef26787adcceec39ea66c6797bcdbd8237a479
clownming <1239064816@qq.com>
2024-02-03 16:36:26 +0800

    优化：保持代码规范将缓存 Key 移至常量类

--- project/src/main/java/com/ming/shortlink/project/mq/producer/ShortLinkStatsSaveProducer.java
+++ project/src/main/java/com/ming/shortlink/project/mq/producer/ShortLinkStatsSaveProducer.java
@@ -1,11 +1,12 @@
 package com.ming.shortlink.project.mq.producer;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
+
+import static com.ming.shortlink.project.common.constant.RedisKeyConstant.SHORT_LINK_STATS_STREAM_TOPIC_KEY;
 
 /**
  * @author clownMing
@@ -17,13 +18,10 @@ public class ShortLinkStatsSaveProducer {
 
     private final StringRedisTemplate stringRedisTemplate;
 
-    @Value("${spring.data.redis.channel-topic.short-link-stats}")
-    private String topic;
-
     /**
      * 发送延迟消费短链接统计
      */
     public void send(Map<String, String> produceMap) {
-        stringRedisTemplate.opsForStream().add(topic, produceMap);
+        stringRedisTemplate.opsForStream().add(SHORT_LINK_STATS_STREAM_TOPIC_KEY, produceMap);
     }
 }
